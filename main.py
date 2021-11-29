e2f78b945ba33ffd8316795e7845eca3a3c8e00d
stulliFugl <sigurdur@fuglar.com>
2021-11-29 10:03:18 +0000

    Changing main file

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ import python_files.calculate_results as calculate_results
 import python_files.consts as consts
 
 
+
 def main():
-    if consts.SETUPWORKOUTS:
-        setup_workouts.setupWorkouts()
-    
-    calculate_results.calculateWorkouts()
+
+    print("1: Setup workouts based on settings")
+    print("2: Calculate workout results based on inputs")
+    val = input("Choose what you wish to do: ")
+
+    if val == '1':
+        if consts.SETUPWORKOUTS == False:
+            print("Setting up workouts is disabled in consts, change and run again")
+        else:
+            if consts.ADDRANDOMSCORES:
+                print("Random scores will be added to the workouts")
+            setup_workouts.setupWorkouts()
+
+    if val == '2':
+        calculate_results.calculateWorkouts()
 
 main()
--- python_files/consts.py
+++ python_files/consts.py
@@ -1,6 +1,6 @@
 # Add constants that can be used everywhere in the program here
 
 DECREMENT = 5 # By how much should each place decrement from total points
-POINTSFORFIRSTPLACE = 100 # Number of points for first place
-SETUPWORKOUTS = False # Creates all the workout files and overrides any existing ones
+POINTSFORFIRSTPLACE = 200 # Number of points for first place
+SETUPWORKOUTS = True # Creates all the workout files and overrides any existing ones
 ADDRANDOMSCORES = True # Adds random scores for testing purposes
