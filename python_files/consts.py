90c9e71c91bd058a90db8f2dd17ef9cb2b85b215
stullifugl <sigurdur@fuglar.com>
2021-11-29 10:19:02 +0000

    Testing out on mac

--- python_files/consts.py
+++ python_files/consts.py
@@ -2,5 +2,5 @@
 
 DECREMENT = 5 # By how much should each place decrement from total points
 POINTSFORFIRSTPLACE = 200 # Number of points for first place
-SETUPWORKOUTS = True # Creates all the workout files and overrides any existing ones
+SETUPWORKOUTS = True # Creates all the workout files and overrides any existing ones if the option is chosen in the terminal
 ADDRANDOMSCORES = True # Adds random scores for testing purposes
